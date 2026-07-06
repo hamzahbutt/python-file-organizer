6f0899f1bd91e9dea9ac3c76e1d6447c247c36dc
Hamzah Butt <hamzahbutt999@gmail.com>
2026-07-06 16:49:47 +0500

    Initial commit: file organizer project

---
+++ README.md
@@ -0,0 +1,38 @@
+# Python File Organizer
+
+A simple Python script that automatically organizes files into folders based on their file extensions.
+
+## Features
+
+- Organizes images
+- Organizes documents
+- Organizes videos
+- Organizes audio
+- Creates folders automatically
+- Writes a log file of moved files
+
+## Technologies Used
+
+- Python
+- os
+- shutil
+
+## How to Run
+
+1. Run the script.
+
+```bash
+python organizer.py
+```
+
+2. Enter the folder path when prompted.
+
+## Supported File Types
+
+| Extension | Folder |
+|-----------|--------|
+| .jpg .jpeg .png .gif | Images |
+| .pdf .docx .txt | Documents |
+| .mp4 .mov | Videos |
+| .mp3 .wav | Audios |
+| .zip | Archives |
---
+++ file_organizer.py
@@ -0,0 +1,98 @@
+import os
+import shutil
+
+foldExt = {
+    # Documents
+    ".pdf": "Documents",
+    ".docx": "Documents",
+    ".doc": "Documents",
+    ".txt": "Documents",
+    ".rtf": "Documents",
+    ".pages": "Documents",
+
+    # Spreadsheets
+    ".xlsx": "Documents",
+    ".xls": "Documents",
+    ".csv": "Documents",
+
+    # Presentations
+    ".pptx": "Documents",
+    ".ppt": "Documents",
+    ".key": "Documents",
+
+    # Images
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+    ".svg": "Images",
+    ".webp": "Images",
+    ".bmp": "Images",
+    ".ico": "Images",
+
+    # Videos
+    ".mp4": "Videos",
+    ".mov": "Videos",
+    ".avi": "Videos",
+    ".mkv": "Videos",
+    ".wmv": "Videos",
+
+    # Audios
+    ".mp3": "Audios",
+    ".wav": "Audios",
+    ".aac": "Audios",
+    ".flac": "Audios",
+    ".m4a": "Audios",
+
+    # Archives
+    ".zip": "Archives",
+    ".rar": "Archives",
+    ".7z": "Archives",
+    ".tar": "Archives",
+    ".gz": "Archives",
+
+    # Executables / Installers
+    ".exe": "Programs",
+    ".msi": "Programs",
+    ".dmg": "Programs",
+    ".pkg": "Programs",
+
+    # Code / Scripts
+    ".py": "Code",
+    ".js": "Code",
+    ".html": "Code",
+    ".css": "Code",
+    ".json": "Code"
+}
+
+while True:
+    src = input("Give the source: ")
+
+    if os.path.isdir(src):
+        break
+    else:
+        print("Invalid path. Please enter a valid folder path.\n")
+
+files = os.listdir(src)
+
+with open(os.path.join(src, "changes.txt"), "w") as file:
+    for f in files:
+        if f == "changes.txt":
+            continue
+        if os.path.isfile(os.path.join(src, f)):
+            ext = os.path.splitext(f)[1]
+            folder = foldExt.get(ext, "Others")
+            Tfile = os.path.join(src, f)
+            target_folder_path = os.path.join(src, folder)
+
+            if not os.path.exists(target_folder_path):
+                os.mkdir(target_folder_path)
+
+            shutil.move(Tfile, target_folder_path)
+
+            file.write("The file " + f + " was moved to " + folder + "\n")
+
+
+
+
+
